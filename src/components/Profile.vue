4da95474fe0330b8071bb066f6cf22e32eec2752
Sujon Ahmed <ahmedsujon050@gmail.com>
2020-02-23 19:02:21 +0600

    Router Parameter

---
+++ src/components/Profile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="profile">
+    <h1>Profile User ID {{ UserId }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "profile",
+  data() {
+    return {
+      UserId: this.$route.params.user_id
+    };
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
